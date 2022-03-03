3d3c20e04f777accb83b3767047c8f8890e0c5fc
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-03 18:43:22 +0500

    initial

---
+++ app/src/main/java/xyz/teamgravity/cropimage/MainActivity.kt
@@ -0,0 +1,18 @@
+package xyz.teamgravity.cropimage
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import xyz.teamgravity.cropimage.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+    }
+}
