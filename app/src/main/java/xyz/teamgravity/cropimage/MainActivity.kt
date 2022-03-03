fcb4ff3b9548883a0585f0dda4419d428890cb78
raheemadamboev <vividricemaryandmax@gmail.com>
2022-03-03 19:04:48 +0500

    cropping the image

--- app/src/main/java/xyz/teamgravity/cropimage/MainActivity.kt
+++ app/src/main/java/xyz/teamgravity/cropimage/MainActivity.kt
@@ -1,18 +1,48 @@
 package xyz.teamgravity.cropimage
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.appcompat.app.AppCompatActivity
+import com.canhub.cropper.CropImageContract
+import com.canhub.cropper.CropImageContractOptions
+import com.canhub.cropper.options
 import xyz.teamgravity.cropimage.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
 
+    private lateinit var imageCropLauncher: ActivityResultLauncher<CropImageContractOptions>
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        launcher()
+        button()
+    }
 
+    private fun launcher() {
+        imageCropLauncher = registerForActivityResult(CropImageContract()) { result ->
+            if (result.isSuccessful) {
+                binding.imageI.setImageURI(result.uriContent)
+            } else {
+                Toast.makeText(this, result.error?.message, Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+
+    private fun button() {
+        onImage()
+    }
+
+    private fun onImage() {
+        binding.imageB.setOnClickListener {
+            imageCropLauncher.launch(options {
+                setAspectRatio(16, 9)
+            })
+        }
     }
 }
